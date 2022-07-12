210c0d281bac8fe8e5efa30d4fc1390786728c84
Cyberkrane <cyberkrane@gmail.com>
2022-07-12 16:02:36 -0300

    carga inicial

---
+++ app.js
@@ -0,0 +1,41 @@
+// variables
+// condicionales
+// ciclos
+// funciones
+
+
+// VARIABLES     TIPOS            CONTENIDO
+
+//           --> string            una letra, una palabra, una frase, un texto; y todo eso siempre debe ir entre comillas simples o dobles da igual.
+
+//           --> number            un numero, una cifra, y nunca el tipo number lleva comillas.
+
+//           --> boolean           este tipo si es binario y solo acepta true o false; nada mas.
+
+//           --> undefined         este tipo hace referencia a una variable que ha sido declarada pero no se le ha asiganado ningun valor.
+
+//           --> null              esto significa que la variable no ha sido declarada. o sea no existe!!!.
+
+//           --> object            variable que contiene un objeto, mas adelante veremos lo que es un objeto.
+
+//           --> array             variable que contiene un array, mas adelante veremos lo que es un array.
+
+ 
+var nombre  = "Maria";
+var edad    = 25;
+var conduce = true;
+var sueldo;
+// var aguinaldo;
+
+
+// CONDICIONALES
+
+if (nombre == "maria") {
+    console.log("hola " + nombre)
+}else{
+    console.log("tu no eres maria")
+}
+
+
+//CICLOS
+//FUNCIONES
