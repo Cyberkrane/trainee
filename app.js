1f5945ccfa3d022318a46b44cc4c7106e3779bf9
Cyberkrane <cyberkrane@gmail.com>
2022-07-13 20:39:35 -0300

    up01

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-// variables
-// condicionales
-// ciclos
-// funciones
+                    //************************************************************ */
+                    //                        VARIABLES                            */
+                    //************************************************************ */
 
 
 // VARIABLES     TIPOS            CONTENIDO
@@ -21,21 +20,44 @@
 //           --> array             variable que contiene un array, mas adelante veremos lo que es un array.
 
  
-var nombre  = "Maria";
-var edad    = 25;
-var conduce = true;
-var sueldo;
-// var aguinaldo;
+var nombre  = "Maria";      // un console.log( nombre ) arrojara el siguiente resultado Maria.
+var edad    = 25;           // un console.log( edad ) arrojara el siguiente resultado 25.
+var conduce = true;         // un console.log( conduce ) arrojara el siguiente resultado true.
+var sueldo;                 // un console.log( sueldo ) arrojara el siguiente resultado undefinded.
+// var aguinaldo;           // un console.log( aguinaldo ) arrojara el siguiente resultado null.
 
 
-// CONDICIONALES
 
-if (nombre == "maria") {
-    console.log("hola " + nombre)
-}else{
-    console.log("tu no eres maria")
+// OPERADORES 
+
+//      =    operador de asignacion         ejemplo:  a la variable nombre le asignamos el valor "Maria".
+
+//      ==   operador de igualdad           ejemplo:  comparamos la igualdad entre los valores de la variable nombre con el valor "maria".
+
+//      ===  operador de igualdad estricta  ejemplo:  podriamos comparar el valor y el tipo de variable al mismo tiempo para ver si son estrictamente iguales en valor y tipo.
+
+
+// EJEMPLOS
+
+// ASIGNACION
+nombre = "Marcos"
+console.log("ASIGNACION => " + nombre)
+
+// COMPARACION DE VALORES
+
+if (nombre == "marcos") {
+    console.log("COMPARACION DE VALORES => coincide")
+} else {
+    console.log("COMPARACION DE VALORES => no coincide")
+}
+
+// COMPARACION DE VALOR Y TIPO DE VARIABLE
+
+if (edad === "25") {
+    console.log("COMPARACION DE VALOR Y TIPO DE VARIABLE => hay coincidencia estricta")
+} else {
+    console.log("COMPARACION DE VALOR Y TIPO DE VARIABLE => no hay coincidencia estricta")
 }
 
 
-//CICLOS
-//FUNCIONES
+// CONSIGNA: hacer que ambas comparaciones arrojen el resulado "coincide" en el caso de la primera comparacion, y "hay coincidencia estricta" en el caso de la segunda.
---
+++ app1.js
@@ -0,0 +1,2 @@
+//CICLOS
+//FUNCIONES
